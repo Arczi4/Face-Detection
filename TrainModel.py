3bbc5d14347cbcfcf3f6a857740121efdf1a04d1
Arczi4 <urzedowski.artur@gmail.com>
2021-05-21 15:23:08 +0200

    add requirements

--- TrainModel.py
+++ TrainModel.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 from os import listdir
 from os.path import isfile, join
-import pickle
 
 Path = './faces/'
 Files = [f for f in listdir(Path) if isfile(join(Path, f))]
